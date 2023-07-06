5d157d3d7481f864f8af4b64de10c141dcaed046
AliJimenez <133895565+AliJimenez@users.noreply.github.com>
2023-07-06 10:15:35 -0500

    Delete Proyecto1P directory

--- Proyecto1P/ProyectoFinal/empleados/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Empleado, Departamento, Proyecto
-
-admin.site.register(Empleado)
-admin.site.register(Departamento)
-admin.site.register(Proyecto)
-# Register your models here.
